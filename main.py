318f7f43476f3ca392728ff089001c705835b099
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-14 10:58:52 +0200

    add spinboxes and edit source template. Add padding

--- main.py
+++ main.py
@@ -5,33 +5,34 @@ window = tkinter.Tk()
 window.title("Invoice generator form")
 
 frame = tkinter.Frame(window)
-frame.pack()
+frame.pack(padx=20, pady=10)
 
-# Program labels:
+
+
+
 invoice_num_label = tkinter.Label(frame, text="Invoice number")
 invoice_num_label.grid(row=0, column=0)
+invoice_num_spinbox = tkinter.Spinbox(frame, from_=1, to=100)
+invoice_num_spinbox.grid(row=1, column=0)
 
 order_num_label = tkinter.Label(frame, text= "Order number")
 order_num_label.grid(row=0, column=1)
+order_num_spinbox = tkinter.Spinbox(frame, from_ =1, to=100)
+order_num_spinbox.grid(row=1, column=1)
 
 issued_date_label = tkinter.Label(frame, text="Issued on")
 issued_date_label.grid(row=0, column=2)
-
-due_date_label = tkinter.Label(frame, text="Due date")
-due_date_label.grid(row=0, column=3)
-
-#Program entry fields:
-
-invoice_num_entry = tkinter.Entry(frame)
-invoice_num_entry.grid(row=1, column=0)
-
-order_num_entry = tkinter.Entry(frame)
-order_num_entry.grid(row=1, column=1)
-
 issued_date_entry = tkinter.Entry(frame)
 issued_date_entry.grid(row=1, column=2)
 
+due_date_label = tkinter.Label(frame, text="Due date")
+due_date_label.grid(row=0, column=3)
 due_date_entry = tkinter.Entry(frame)
 due_date_entry.grid(row=1, column=3)
+
+
+
+
+
 
 window.mainloop()
