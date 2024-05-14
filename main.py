dcceca37f16b2699c01479a41aeec8327d1c8bd6
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-14 11:40:26 +0200

    edit row numbers, edit add item button to span over the whole window, add generate invoice and new invoice buttons

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ window.title("Invoice generator form")
 frame = tkinter.Frame(window)
 frame.pack(padx=20, pady=10)
 
-
+#Labels and entry fields
 invoice_num_label = tkinter.Label(frame, text="Invoice number")
 invoice_num_label.grid(row=0, column=0)
 invoice_num_spinbox = tkinter.Spinbox(frame, from_=1, to=100)
@@ -17,6 +17,9 @@ order_num_label = tkinter.Label(frame, text= "Order number")
 order_num_label.grid(row=0, column=1)
 order_num_spinbox = tkinter.Spinbox(frame, from_ =1, to=100)
 order_num_spinbox.grid(row=1, column=1)
+
+rws_label = tkinter.Label(frame, text="RWS")
+rws_label.grid(row=1, column=1)
 
 issued_date_label = tkinter.Label(frame, text="Issued on")
 issued_date_label.grid(row=0, column=2)
@@ -28,13 +31,23 @@ due_date_label.grid(row=0, column=3)
 due_date_entry = tkinter.Entry(frame)
 due_date_entry.grid(row=1, column=3)
 
+# Add item button
 add_item_button = tkinter.Button(frame, text="Add item")
-add_item_button.grid(row=2, column=3, pady=10)
+add_item_button.grid(row=3, column=0, columnspan=4, sticky="news", pady=10)
 
-columns = ('PO', 'Project', 'Price')
+# Tree
+columns = ('po', 'project', 'price')
 tree = ttk.Treeview(frame, columns=columns, show="headings")
-tree.grid(row=3, column=0, columnspan=4, padx=20, pady=10)
+tree.grid(row=4, column=0, columnspan=4, padx=20, pady=10)
+tree.heading("po", text="PO")
+tree.heading("project", text="Project")
+tree.heading("price", text="Price")
 
+generate_invoice_button = tkinter.Button(frame, text="Generate invoice")
+generate_invoice_button.grid(row=5, column=0, columnspan=4, sticky= "news", padx=20, pady=10)
+
+new_invoice_button = tkinter.Button(frame, text="Generate Invoice")
+new_invoice_button.grid(row=6, column=0, columnspan=4, sticky="news", padx=20, pady=5)
 
 
 
