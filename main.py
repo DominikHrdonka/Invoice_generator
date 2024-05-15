8c2e91bd7da6cf5c2c9c9aab392a4a73ba72ff49
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-15 11:05:25 +0200

    create the main menu to choose from RWS and English invoices. Each choice opens a different window containing a different program according to the given invoice rules.

--- main.py
+++ main.py
@@ -1,125 +1,30 @@
 import tkinter
 from tkinter import ttk
-from docxtpl import DocxTemplate
-from tkinter import messagebox
+from RWS import program1
 
-def main():
+def rws_invoice():
+    program1()
 
-    invoice_list = []
-    def add_item():
-        po = int(po_entry.get())
-        project = project_entry.get()
-        price= float(price_entry.get())
-        invoice_item = [po, project, price]
-
-        tree.insert('', 0, values=invoice_item)
-        clear_item()
-        invoice_list.append(invoice_item)
-    
-    def clear_item():
-        po_entry.delete(0, tkinter.END)
-        project_entry.delete(0, tkinter.END)
-        price_entry.delete(0, tkinter.END)
-        price_entry.insert(0, "0.0")
+def en_invoice():
+    print("English invoice")
         
 
-    def new_invoice():
-        invoice_num_spinbox.delete(0, tkinter.END)
-        order_num_spinbox.delete(0, tkinter.END)
-        issued_date_entry.delete(0, tkinter.END)
-        due_date_entry.delete(0, tkinter.END)
-        clear_item()
-        tree.delete(*tree.get_children())
-        invoice_list.clear()
-    
-    def generate_invoice():
-        doc = DocxTemplate("/root/workspace/github.com/DominikHrdonka/Invoice_generator/INVOICE_template_RWS.docx")
-        invoice_num = invoice_num_spinbox.get()
-        order_num = order_num_spinbox.get()
-        issued_date = issued_date_entry.get()
-        due_date = due_date_entry.get()
-        total_price = sum(item[2] for item in invoice_list) 
+# GUI
+window = tkinter.Tk()
+window.title("Invoice generator form")
 
-        doc.render(
-            {
-                "invoice_num": invoice_num,
-                "order_num": order_num,
-                "date": issued_date,
-                "due_date": due_date,
-                "invoice_list": invoice_list,
-                "total_price": total_price
-            }
-        )
-        doc_name = "new_invoice_" + "RWS - "+ invoice_num + "_" + order_num
-        doc.save(doc_name)
-        messagebox.showinfo("Invoice Complete", "Invoice Complete") 
-        new_invoice()
+frame = tkinter.Frame(window)
+frame.pack(padx=20, pady=10)
 
+#Labels and entry fields
+rws_invoice_label = tkinter.Label(frame, text="Choose invoice")
+rws_invoice_label.grid(row=0, column=0)
+rws_invoice_button = tkinter.Button(frame, text= "RWS Invoice", command= rws_invoice)
+rws_invoice_button.grid(row=1, column=0)
 
+en_invoice_label = tkinter.Label(frame, text="Choose invoice")
+en_invoice_label.grid(row=2, column=0)
+en_invoice_button = tkinter.Button(frame, text= "English Invoice", command= en_invoice)
+en_invoice_button.grid(row=3, column=0)
 
-
-
-    window = tkinter.Tk()
-    window.title("Invoice generator form")
-
-    frame = tkinter.Frame(window)
-    frame.pack(padx=20, pady=10)
-
-    #Labels and entry fields
-    invoice_num_label = tkinter.Label(frame, text="Invoice number")
-    invoice_num_label.grid(row=0, column=0)
-    invoice_num_spinbox = tkinter.Spinbox(frame, from_=1, to=100)
-    invoice_num_spinbox.grid(row=1, column=0)
-
-    order_num_label = tkinter.Label(frame, text= "Order number")
-    order_num_label.grid(row=0, column=1)
-    order_num_spinbox = tkinter.Spinbox(frame, from_ =1, to=100)
-    order_num_spinbox.grid(row=1, column=1)
-
-    issued_date_label = tkinter.Label(frame, text="Issued on")
-    issued_date_label.grid(row=0, column=2)
-    issued_date_entry = tkinter.Entry(frame)
-    issued_date_entry.grid(row=1, column=2)
-
-    due_date_label = tkinter.Label(frame, text="Due date")
-    due_date_label.grid(row=0, column=3)
-    due_date_entry = tkinter.Entry(frame)
-    due_date_entry.grid(row=1, column=3)
-
-    po_label = tkinter.Label(frame, text="PO")
-    po_label.grid(row=3, column=0)
-    po_entry = tkinter.Entry(frame)
-    po_entry.grid(row=4, column=0)
-
-    project_label = tkinter.Label(frame, text="Project")
-    project_label.grid(row=3, column=1)
-    project_entry = tkinter.Entry(frame)
-    project_entry.grid(row=4, column=1)
-
-    price_label = tkinter.Label(frame, text="Price")
-    price_label.grid(row=3, column=2)
-    price_entry = tkinter.Entry(frame)
-    price_entry.grid(row=4, column=2)
-
-    # Add item button
-    add_item_button = tkinter.Button(frame, text="Add item", command= add_item)
-    add_item_button.grid(row=5, column=0, columnspan=4, sticky="news", pady=10)
-
-    # Tree
-    columns = ('po', 'project', 'price')
-    tree = ttk.Treeview(frame, columns=columns, show="headings")
-    tree.grid(row=6, column=0, columnspan=4, padx=20, pady=10)
-    tree.heading("po", text="PO")
-    tree.heading("project", text="Project")
-    tree.heading("price", text="Price")
-
-    generate_invoice_button = tkinter.Button(frame, text="Generate invoice", command=generate_invoice)
-    generate_invoice_button.grid(row=7, column=0, columnspan=4, sticky= "news", padx=20, pady=10)
-
-    new_invoice_button = tkinter.Button(frame, text="New Invoice", command = new_invoice)
-    new_invoice_button.grid(row=8, column=0, columnspan=4, sticky="news", padx=20, pady=5)
-
-
-
-    window.mainloop()
-main()
+window.mainloop()
