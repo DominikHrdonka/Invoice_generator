84188aaaa85dab72f16a28d37534dd1665e771c6
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-17 11:42:37 +0200

    create calendar widget and button to select the date

--- date_picker.py
+++ date_picker.py
@@ -1,10 +1,17 @@
 import tkinter
 from tkinter import ttk
-from tkcalendar import *
+from tkcalendar import Calendar
 
 root = tkinter.Tk()
 root.title("Calendar")
 root.geometry("300x300")
+
+cal = Calendar(
+    root, selectmode = 'day',
+               year = 2024, month = 5,
+               day = 17)
+
+cal.pack(pady=20)
 
 
 select_date_button = tkinter.Button(text="Select date")
