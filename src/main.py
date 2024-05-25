7d4bc872e02a9d234ea53fa53cae36b92a871530
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-25 09:36:53 +0200

    minor change in function calling in main.py

--- src/main.py
+++ src/main.py
@@ -6,14 +6,6 @@ from summary import (
     open_summary,
 )
 
-
-def rws_invoice():
-    rws()
-
-def en_invoice():
-    en()
-
-
 # GUI
 window = tkinter.Tk()
 window.title("Invoice generator form")
@@ -26,10 +18,10 @@ frame.pack(padx=20, pady=10)
 choose_invoice_label = tkinter.Label(frame, text="Choose invoice:")
 choose_invoice_label.grid(row=0, column=0, columnspan=1)
 
-rws_invoice_button = tkinter.Button(frame, text= "RWS Invoice", command= rws_invoice)
+rws_invoice_button = tkinter.Button(frame, text= "RWS Invoice", command= rws)
 rws_invoice_button.grid(row=1, column=0, padx=5)
 
-en_invoice_button = tkinter.Button(frame, text= "English Invoice", command= en_invoice)
+en_invoice_button = tkinter.Button(frame, text= "English Invoice", command= en)
 en_invoice_button.grid(row=1, column=1)
 
 open_summary_button = tkinter.Button(frame, text="Open summary", command= open_summary)
--- src/shared.py
+++ src/shared.py
@@ -1,2 +1,1 @@
 selected_date = None
-this_year_total_sum = 120497.55
