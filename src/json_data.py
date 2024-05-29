05430f7f55fb01221cff920b55bb455e365b273b
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-05-29 10:51:30 +0200

    write logic to create a new json file with shared data if it doesn't exist. Write function to update shared_data values, especially next_invoice_number

--- src/json_data.py
+++ src/json_data.py
@@ -47,7 +47,26 @@ def save_totals(value):
 	with open("stored_totals.json", "w") as outfile:
 		json.dump(stored_totals_data, outfile, indent=4)
 
-	
+
+
 shared_data_dictionary = {
 	"last_invoice_num": 17,
 }
+
+# If not exists, create json file
+if not os.path.exists('shared_data.json'):
+	shared_data_obj = json.dumps(shared_data_dictionary, indent=4)
+
+	with open('shared_data.json', 'w') as file:
+		file.write(shared_data_obj)
+
+# Open existing json file
+with open('shared_data.json', 'r') as file:
+	shared_data = json.load(file)
+
+def update_next_invoice_num():
+	shared_data['last_invoice_num'] += 1
+	
+	#Writing back the updated date
+	with open('shared_data.json', 'w') as outfile:
+		json.dump(shared_data, outfile, indent=4)
