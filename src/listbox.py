130313708ede349c2859fe1aeba9c7648a4e41fc
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-09-09 21:28:28 +0200

    create logic to insert clients into the listbox

--- src/listbox.py
+++ src/listbox.py
@@ -9,13 +9,17 @@ window.geometry("300x300")
 frame = tkinter.Frame(window)
 frame.pack(padx=20, pady= 10)
 
-
 Lb1 = Listbox(frame)
 Lb1.pack()
 
-Lb1.insert(1, "Jan")
-Lb1.insert(2, "Vera")
-Lb1.insert(3, "Petr")
-
+"""
+Inserting clients to the listbox.
+clients list will be replaced by the actualy list of clients from DB
+"""
+clients = ["Petr", "Jana", "Vera", "Tom"]
+order = 0
+for client in clients:
+    Lb1.insert(order, client)
+    order+=1
 
 window.mainloop()
