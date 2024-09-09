a2a2fade199f1064eae669ddf7bfc360fd8b7459
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-09-09 21:19:21 +0200

    give size to window and pack Lb1 to show in the GUI

--- src/listbox.py
+++ src/listbox.py
@@ -4,12 +4,14 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("Client list")
+window.geometry("300x300")
 
 frame = tkinter.Frame(window)
 frame.pack(padx=20, pady= 10)
 
 
-Lb1 = Listbox(window)
+Lb1 = Listbox(frame)
+Lb1.pack()
 
 Lb1.insert(1, "Jan")
 Lb1.insert(2, "Vera")
