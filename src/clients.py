a2cebc798e4ef5f709a80e299cfbfee25042f875
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-09-11 20:59:40 +0200

    create function to add client to client_list table of the DB

--- src/clients.py
+++ src/clients.py
@@ -1,22 +1,36 @@
 import tkinter
 from tkinter import (ttk)
+import sqlite3
 
 def open_clients():
-    root = tkinter.Tk()
-    root.title("Clients")
-    root.geometry("600x300")
+    def main():
+        
+        # GUI
+        root = tkinter.Tk()
+        root.title("Clients")
+        root.geometry("600x300")
 
-    frame = tkinter.Frame(root)
-    frame.pack(padx=20, pady=10)
+        frame = tkinter.Frame(root)
+        frame.pack(padx=20, pady=10)
 
-    clients_columns = ('name', 'email', 'hourly_rate')
-    clients_treeview = ttk.Treeview(frame, columns=clients_columns, show="headings")
-    clients_treeview.grid(row=0, column=0)
-    clients_treeview.heading('name', text='Name')
-    clients_treeview.heading('email', text='E-mail')
-    clients_treeview.heading('hourly_rate', text='Hourly rate')
+        clients_columns = ('name', 'email', 'hourly_rate')
+        clients_treeview = ttk.Treeview(frame, columns=clients_columns, show="headings")
+        clients_treeview.grid(row=0, column=0)
+        clients_treeview.heading('name', text='Name')
+        clients_treeview.heading('email', text='E-mail')
+        clients_treeview.heading('hourly_rate', text='Hourly rate')
 
-    add_client_button = tkinter.Button(frame, text="Add Client", command=None)
-    add_client_button.grid(row=1, column=0, padx=5, pady=5, sticky='news')
+        add_client_button = tkinter.Button(frame, text="Add Client", command=None)
+        add_client_button.grid(row=1, column=0, padx=5, pady=5, sticky='news')
 
-    root.mainloop()
+        #Functions
+
+        def add_client(name, email, hourly_rate):
+            with sqlite3.connect('clients.db') as connection:
+                cursor = connection.cursor()
+                cursor.execute(
+                    "INSERT INTO client_list (name, email, hourly_rate) VALUES (?,?,?);" (name, email, hourly_rate)
+                )
+
+        root.mainloop()
+    main()
