f953eacb1485a2de81d2e71a4b0962d4252d79cf
Dominik Hrdonka <hrdonka.d@gmail.com>
2025-02-21 10:18:26 +0100

    create seperate databases and json_files directories in working directory. Create variables in shared.py to store absolute paths to both databases

--- src/shared.py
+++ src/shared.py
@@ -2,7 +2,11 @@ import os
 selected_date = None
 selected_client = None
 
-base_json_files_path = "/root/workspace/github.com/DominikHrdonka/Invoice_generator/src/"
+base_json_files_path = "/root/workspace/github.com/DominikHrdonka/Invoice_generator/json_files/"
+base_databases_path = "/root/workspace/github.com/DominikHrdonka/Invoice_generator/databases/"
 
 stored_totals_json_path = os.path.join(base_json_files_path, 'stored_totals.json')
 shared_data_json_path = os.path.join(base_json_files_path, 'shared_data.json')
+
+clients_db_path = os.path.join(base_databases_path, 'clients.db')
+invoices_db_path = os.path.join(base_databases_path, 'invoices.db')
